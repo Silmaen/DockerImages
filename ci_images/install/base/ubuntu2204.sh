317f4f6b6cfe90598348cf3e4ba0c6850468b98f
Silmaen <genteur.slayer@laposte.net>
2024-01-13 23:52:48 +0100

    update needed package for CI

--- ci_images/install/base/ubuntu2204.sh
+++ ci_images/install/base/ubuntu2204.sh
@@ -32,7 +32,7 @@ install_package curl gpg ca-certificates
 update_package_list
 
 # Install base packages
-install_package python3 python3-lxml python3-jinja2 python3-pip python3-requests-toolbelt p7zip unzip time patchelf cmake cmake-data make ninja-build ccache doxygen graphviz mold
+install_package python3 python3-future python3-lxml python3-jinja2 python3-pip python3-requests-toolbelt p7zip unzip time patchelf cmake cmake-data make ninja-build ccache doxygen graphviz mold
 
 # create a default cache dir
 [ ! -e /tmp/cache_dir ] && install -d -m 0755 -o user -g user /tmp/cache_dir
