9fddc0e090f6316e97e0570f463ceb8867ae4db0
Silmaen <genteur.slayer@laposte.net>
2024-01-16 21:24:51 +0100

    update needed package for CI

--- ci_images/install/base/ubuntu2310.sh
+++ ci_images/install/base/ubuntu2310.sh
@@ -32,7 +32,7 @@ useradd -m user
 update_package_list
 
 # Install base packages
-install_package python3 python3-future python3-lxml python3-jinja2 python3-pip python3-requests-toolbelt p7zip unzip time patchelf cmake cmake-data make ninja-build ccache doxygen graphviz mold gcovr
+install_package python3 python3-future python3-lxml python3-jinja2 python3-pip python3-requests-toolbelt p7zip unzip time patchelf cmake cmake-data make ninja-build ccache doxygen graphviz mold gcovr git
 
 # create a default cache dir
 [ ! -e /tmp/cache_dir ] && install -d -m 0755 -o user -g user /tmp/cache_dir
