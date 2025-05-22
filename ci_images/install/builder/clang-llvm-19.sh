cde2217e0fed1a45c62eabccb50193b8cf3e9ef8
Silmaen <genteur.slayer@laposte.net>
2025-05-22 23:21:36 +0200

    add mor compiler support

---
+++ ci_images/install/builder/clang-llvm-19.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+# Stop if error
+set -e
+
+CLANG_VERSION=19
+
+function update_package_list() {
+  DEBIAN_FRONTEND=noninteractive apt update
+}
+function install_package() {
+  DEBIAN_FRONTEND=noninteractive apt install --no-install-recommends -y $@
+}
+function clear_cache() {
+  DEBIAN_FRONTEND=noninteractive apt autoremove
+  rm -rf /var/cache/apt/archive/* /var/lib/apt/lists/*
+}
+
+. /etc/os-release && curl -fsSL https://apt.llvm.org/llvm-snapshot.gpg.key | gpg --dearmor > /usr/share/keyrings/llvm-archive-keyring.gpg && \
+echo "deb [signed-by=/usr/share/keyrings/llvm-archive-keyring.gpg] https://apt.llvm.org/${UBUNTU_CODENAME}/ llvm-toolchain-${UBUNTU_CODENAME}-${CLANG_VERSION} main" > /etc/apt/sources.list.d/llvm.list
+
+# update package list
+update_package_list
+
+# Install base packages
+install_package {clang,lld,llvm,clang-tidy}-${CLANG_VERSION} libclang*-${CLANG_VERSION}-dev
+
+update-alternatives --install /usr/bin/lld lld /usr/bin/lld-${CLANG_VERSION} ${CLANG_VERSION}
+update-alternatives --install /usr/bin/clang clang /usr/bin/clang-${CLANG_VERSION} ${CLANG_VERSION}
+update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-${CLANG_VERSION} ${CLANG_VERSION}
+update-alternatives --install /usr/bin/clang-tidy clang-tidy /usr/bin/clang-tidy-${CLANG_VERSION} ${CLANG_VERSION}
+update-alternatives --install /usr/bin/llvm-cov llvm-cov /usr/bin/llvm-cov-${CLANG_VERSION} ${CLANG_VERSION}
+
+# Clear the caches
+clear_cache
